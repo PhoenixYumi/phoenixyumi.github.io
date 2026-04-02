caab89b731f1398a6b64464fcc45008e5f377fd0
PhoenixYumi <phoenixyumi15@gmail.com>
2026-04-01 21:53:02 -0500

    Create index.js

---
+++ functions/index.js
@@ -0,0 +1,27 @@
+export async function onRequest(context) {
+  const { env } = context;
+  
+  // These pull from the "Environment Variables" we are about to set
+  const NOTION_TOKEN = env.NOTION_TOKEN;
+  const DATABASE_ID = env.DATABASE_ID;
+
+  const response = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${NOTION_TOKEN}`,
+      "Notion-Version": "2022-06-28",
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  return new Response(JSON.stringify(data), {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "*"
+    },
+  });
+}
